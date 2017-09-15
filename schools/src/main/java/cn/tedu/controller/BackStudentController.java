4237e0c95cae7f656d3c857b4773e5f8074ce232
wanganqiwanganqi <31953087+wanganqiwanganqi@users.noreply.github.com>
2017-09-15 17:14:40 +0800

    更改service

--- schools/src/main/java/cn/tedu/controller/BackStudentController.java
+++ schools/src/main/java/cn/tedu/controller/BackStudentController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import cn.tedu.pojo.User;
@@ -18,9 +19,10 @@ public class BackStudentController {
 	private BackStudentService backStudentService;
 	//跳转到学生管理界面
 	@RequestMapping("student")
-	public String toStudent(){
+	public String toStudent(Model model){
 		//从数据库里查询所有学生
 		List<User> userList = backStudentService.findAllStudent();
+		model.addAttribute("userList", userList);
 		return "back/student";
 	}
 	
