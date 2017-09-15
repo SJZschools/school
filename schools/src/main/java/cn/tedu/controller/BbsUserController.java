1aacd0dd4bc92fbaaa6f82c4446d0b33f9d5926d
duanmenglin1988 <31951388+duanmenglin1988@users.noreply.github.com>
2017-09-15 19:25:44 +0800

    dml9-15/19

--- schools/src/main/java/cn/tedu/controller/BbsUserController.java
+++ schools/src/main/java/cn/tedu/controller/BbsUserController.java
@@ -1,5 +1,31 @@
 package cn.tedu.controller;
 
-public class BbsUserController {
+import java.util.UUID;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import cn.tedu.pojo.Bbs;
+import cn.tedu.pojo.User;
+import cn.tedu.service.UserService;
+
+@Controller
+public class BbsUserController {
+	@Autowired
+	private UserService userService;
+	@RequestMapping("/save")
+	public String saveBBS(Bbs bbs,HttpSession session){
+		User user=new User();
+		user.setId("123");
+		session.setAttribute("userSession", user);
+		
+		String bId=UUID.randomUUID().toString();
+		bbs.setBssId(bId);
+		User uu=(User) session.getAttribute("userSession");
+		bbs.setCreatId(uu.getId());
+		return "/bbs_home";
+	}
 }
