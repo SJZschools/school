9582400f8b896dc51220553231121aafb9c8564a
lijil0708 <31958138+lijil0708@users.noreply.github.com>
2017-09-16 14:22:47 +0800

    lijil
    day 2017-9-16

--- schools/src/main/java/cn/tedu/mapper/DynamicMapper.java
+++ schools/src/main/java/cn/tedu/mapper/DynamicMapper.java
@@ -6,7 +6,12 @@ import cn.tedu.pojo.Dynamic;
 
 public interface DynamicMapper {
 	
-	//查询所有动态信息
-	List<Dynamic> findAll();
+	//查询所有动态信息by日期
+	List<Dynamic> findAllByTime();
+	//查询所有动态信息by点赞
+	List<Dynamic> findAllByRcount();
+	//展示详细内容页面
+	Dynamic findDynamicById(String dynamicId);
+	
 
 }
